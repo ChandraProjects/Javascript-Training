48a9aed9b8a7b4cde98c1a254d8e9f618b512e7c
chandra <cm495p@gmail.com>
2020-05-10 11:57:26 +0530

    removed firebase

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+    (async function () {
+        await fetch('./menu.json').then(res => res.json())
+            .then(response => {
+                createList(response);
+            })
+    })();
 
-(async function () {
-    await fetch('./menu.json').then(res => res.json())
-        .then(response => {
-            createList(response);
-        })
-})();
-
-function createList(response) {
-    for (let key in response) {
-        let ul = document.createElement('ul');
-        let h3 = document.createElement('h3');
-        let titleTab = document.createElement('strong');
-        titleTab.innerText = key;
-        h3.appendChild(titleTab)
-        ul.appendChild(h3);
-        response[key].map(element => {
-            let li = document.createElement('li');
-            let a = document.createElement('a');
-            a.setAttribute('href', element.url);
-            a.innerText = element.title;
-            li.appendChild(a);
-            ul.appendChild(li);
-        });
-        document.getElementById('menu_list').appendChild(ul);
+    function createList(response) {
+        for (let key in response) {
+            let ul = document.createElement('ul');
+            let h3 = document.createElement('h3');
+            let titleTab = document.createElement('strong');
+            titleTab.innerText = key;
+            h3.appendChild(titleTab)
+            ul.appendChild(h3);
+            response[key].map(element => {
+                let li = document.createElement('li');
+                let a = document.createElement('a');
+                a.setAttribute('href', element.url);
+                a.innerText = element.title;
+                li.appendChild(a);
+                ul.appendChild(li);
+            });
+            document.getElementById('menu_list').appendChild(ul);
+        }
     }
-}
+})
